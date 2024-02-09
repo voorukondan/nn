424491aaa261b0997c1152851423a099ed39f084
voorukondan <voorukondan@gmail.com>
2024-02-09 16:36:43 +0000

    solution

---
+++ src/main/java/com/example/song/Song.java
@@ -0,0 +1,44 @@
+// Write your code here
+package class Song {
+    private int songId;
+    private String songName;
+    private String lyricist;
+    private String singer;
+    private String musicDirector;
+
+    public Song(int songId, String songName, String lyricist, String singer, String musicDirector) {
+        this.songId = songId;
+        this.songName= songName;
+        this.lyricist = lyricist;
+        this.singer = singer;
+        this.musicDirector = musicDirector;
+    }
+    public int getsongId() {
+        return songId;
+    }
+    public void setsongId(int songId) {
+        this.songId = songId;
+    }
+    public String getSongName() {
+        return songName;
+    }
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+    public String getLyricist() {
+        return lyricist;
+    }
+    public void setLyricist(String lyricist) {
+        this.lyricist= lyricist;
+    }
+    public String getSinger() {
+        return singer;
+    }
+    public String getMusicDirector() {
+        return musicDirector;
+    }
+    public void setMusicDirector(String musicDirector) {
+        this.musicDirector = musicDirector;
+    }
+    
+}
